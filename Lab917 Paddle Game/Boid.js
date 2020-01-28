60a1c995e7e68db24c429defdb980be5a696803d
797903 <54591322+797903@users.noreply.github.com>
2020-01-28 08:50:43 -0800

    yes

---
+++ Lab917 Paddle Game/Boid.js
@@ -0,0 +1,31 @@
+class Boid{
+  constructor(x, y, dx, dy){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+  }
+  run(){
+
+  }
+  update(){
+    
+  }
+
+  checkEdges(){
+    if(this.loc.x < 0 || this.loc.x > width){
+      this.vel.dx = -1*this.vel.dx;
+    }
+    if(this.loc.y < 0 || this.loc.y > height){
+      this.vel.dx = -1*this.vel.dx;
+  }
+  render(){
+    for(var i=0; i<boids.length[]-1; i++){
+      var distance = dist(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
+      if(distance<200 && distance !==0){
+        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
+      }
+    }
+  }
+
+
+
+}
