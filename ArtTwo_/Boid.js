bf9f651f23105071c027bb7e7b812e182d5f756c
797903 <54591322+797903@users.noreply.github.com>
2020-01-31 08:29:09 -0800

    test

---
+++ ArtTwo_/Boid.js
@@ -0,0 +1,38 @@
+class Boid{
+  constructor(x, y, dx, dy){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+  }
+  run(){
+    //console.log("Peace");
+    this.update();
+    this.checkEdges();
+    this.render();
+  }
+  update(){
+    this.loc.add(this.vel);
+  }
+
+  checkEdges(){
+    if(this.loc.x < 0 || this.loc.x > width){
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.y < 0 || this.loc.y > height){
+      this.vel.y = -1*this.vel.y;
+  }
+}
+  render(){
+    var j = 100;
+    opacity(j);
+    stroke(random(255), random(255), random(255));
+    strokeWeight(0.25);
+
+    for(var i=0; i<boids.length; i++){
+      opacity(j--);
+      var distance = this.loc.dist(boids[i].loc);
+      if(distance < 200 && distance !==0){
+        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
+      }
+    }
+  }
+}
