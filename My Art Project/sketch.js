29bbcd82be7d77a2821fec46faa5ba83882c0eba
797903 <54591322+797903@users.noreply.github.com>
2020-02-05 08:47:49 -0800

    art project

---
+++ My Art Project/sketch.js
@@ -0,0 +1,59 @@
+// Garrett Faure
+// Feb. 5, 2020
+
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(0);
+  mousePressed(0, 0);
+}
+
+function draw() {
+  background(0);
+  j = 0;
+  while(++j<num) {
+    target = pos[j];
+    target.aX *= .98;
+    target.aY *= .98;
+
+    oldX = target.x;
+    oldY = target.y;
+    target.x += target.aX;
+    target.y += target.aY + 1;
+
+    target.d *= .98;
+    w = target.d*5;
+    i = 0;
+
+    while(i++<5) {
+      base = 50/i;
+      push();
+      blendMode(ADD);
+      stroke(base + (1-target.d)*100, base+50, base + target.d*250, base);
+      strokeWeight(w*=1.5);
+      line(target.x, target.y, oldX, oldY);
+      pop();
+    }
+  }
+}
+
+function mousePressed(e, setD=1) {
+  pos = [];
+  num = 100;
+  speed = 10;
+  m = mouseX;
+  n = mouseY;
+  i = 0;
+  while(++i<num) {
+		a = createVector(random(speed)).rotate(random()*TAU);
+    pos[i] = {
+      x: m,
+      y: n,
+      aX: a.x,
+      aY: a.y,
+      d: random(setD*.5, setD)
+    }
+  }
+}
+/**/
