be3c52b1b2eafed24e51a7c77755021e95ec434b
797903 <54591322+797903@users.noreply.github.com>
2020-03-04 08:51:51 -0800

    trying to make repulsion

--- __a Fall Final new/Ball.js
+++ __a Fall Final new/Ball.js
@@ -14,26 +14,41 @@ class Ball{
   }
 
   update(){
+    this.loc.add(this.vel);
+    this.vel.limit(4);
     var distToShip;
     // this.vel.add(this.acc)
       distToShip = this.loc.dist(ships.loc); //distance to the ship
-      if(distToShip < 100){ //distance to planet
+      if(distToShip < 50){ //distance to planet
         this.loc.x = random(0, 500);
         this.loc.y = random(0, 500);
       }
-    if(){
-
       // increases velocity based on direction traveling in.
-      if(distToShip < 175){
-        this.loc.add(Ship.vel*0.90);
+        if(distToShip < 175){
+          this.vel.x = Ship.vel.dx;
+          this.vel.y = Ship.vel.dy;
       }
-    }
 
   //     for(var i = 0; i < n; i++){ // no overlapping
   //       for(var j = 0; j < n; j++){
   //       if(i != j && bigFella[i]
   //     }
   //   }
+  }
+
+  checkEdges(){
+      if(this.loc.x < 0){
+        this.vel.x = -this.vel.x
+      }
+      if(this.loc.x > width){
+        this.vel.x = -this.vel.x
+      }
+      if(this.loc.y < 0){
+        this.vel.y = -this.vel.y
+      }
+      if(this.loc.y > height){
+        this.vel.y = -this.vel.y
+    }
   }
 
   render(){
